ad06580ffe98ced0ec2c1efc1c0864982ffc64e3
krishnaa42342 <krishnaa423@rogkrishnaa>
2024-08-20 14:36:50 -0400

    Added abacus

---
+++ down_abacus.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker compose -f dc_run.yml down fp-abacus-linux-amd64
--- sample.py
+++ sample.py
@@ -1,14 +1,44 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QLabel
+#region: Modules.
+import numpy as np
+from mpi4py import MPI  
+from petsc4py import PETSc
+from slepc4py import SLEPc
+#endregion
 
-app = QApplication(sys.argv)
+#region: Variables.
+#endregion
 
-import pyvista as pv
-from pyvistaqt import BackgroundPlotter
+#region: Functions.
+def main():
+    comm = MPI.COMM_WORLD
+    mpi_rank = comm.Get_rank()
+    A = PETSc.Mat().create()
+    A.setType(PETSc.Mat.Type.MPIDENSE)
+    A.setSizes((4, 4))
+    A.setUp()
+    A[range(4), range(4)] = np.arange(16).reshape(4, 4)
+    A.assemble()
 
-sphere = pv.Sphere()
+    print(f'Rank: {mpi_rank}, A: {A.getDenseArray()}')
 
-plotter = BackgroundPlotter()
-plotter.add_mesh(sphere, show_edges=True)
+    eps = SLEPc.EPS().create()
+    eps.setProblemType(SLEPc.EPS.ProblemType.NHEP)
+    eps.setOperators(A)
+    eps.setUp()
 
-sys.exit(app.exec_())
+    eps.solve()
+
+    num_eigs = eps.getConverged()
+
+    for eig_idx in range(num_eigs):
+        if mpi_rank==0:
+            print(f'eig_idx: {eig_idx}, eig_value: {eps.getEigenvalue(eig_idx)}')
+#endregion
+
+#region: Classes.
+#endregion
+
+#region: Main.
+if __name__=='__main__':
+    main()
+#endregion
